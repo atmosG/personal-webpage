926deb7470a26ad7efe09668d86215643f49029a
atmosG <86365959+atmosG@users.noreply.github.com>
2021-11-13 18:56:58 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
     $('.menu-toggler').click(function() {
         $(this).toggleClass("active");
         $(".navbar-menu").toggleClass("active");
-    })
+    });
+
+    $(".works").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {enabled:true}
+    });
 });
