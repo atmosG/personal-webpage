110d9ca84d60bc63cc0f780b42d285e176252401
atmosG <86365959+atmosG@users.noreply.github.com>
2021-11-18 09:08:08 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         if(this.scrollY > 20)
-            $(".navbar").addClass("sticky");
-        else
+            {$(".navbar").addClass("sticky");
+            $(".goTop").fadeIn();}
+        else{
             $(".navbar").removeClass("sticky");
+            $(".goTop").fadeOut();}
     });
+    $(".goTop").click(function() {scroll(0,0)});
 
     $('.menu-toggler').click(function() {
         $(this).toggleClass("active");
